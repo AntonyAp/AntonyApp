92a9fb7113838309e4d06d1c5ec283b1b4aa7271
anton <anton@DESKTOP-KPB8I4G>
2018-09-27 20:32:16 +0300

    Created 2 projects

---
+++ Web/Controllers/AutorizationController.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Linq;
+using System.Web.Mvc;
+using testtest.Models;
+
+namespace testtest.Controllers
+{
+       public class AutorizationController : Controller
+    {
+        UserContext db = new UserContext();
+        // GET: Autorization
+        public ActionResult Login(User model)
+        {
+            var users = db.Users;
+            var userExists = users.Any(x => x.Login == model.Login && x.Password == model.Password);
+            var redirectViewName = userExists ? "Football" : "LoginPage";
+                return RedirectToAction(redirectViewName, "Autorization");
+            }
+
+            public ActionResult LoginPage()
+        {
+            return View();
+        }
+
+            public ActionResult Football()
+        {
+       
+            return View();
+        }
+    }
+}
---
+++ Web/Controllers/RegistrationController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using testtest.Models;
+using System.Data.Entity;
+
+namespace testtest.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private UserContext db = new UserContext();
+        // GET: Registration
+        public ActionResult RegistrationPage()
+        {
+            return View();
+        }
+        public ActionResult Registration( User user)
+        {
+            db.Users.Add(user);
+            db.SaveChanges();
+            return RedirectToAction("LoginPage", "Autorization");
+        }
+    }
+}
