c28b9c4e13386e23c136fbb43355e8f1f5f102de
anton <anton@DESKTOP-KPB8I4G>
2018-09-28 23:02:13 +0300

    Registration and autorization are working

--- Web/Controllers/RegistrationController.cs
+++ Web/Controllers/RegistrationController.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using testtest.Models;
-using System.Data.Entity;
-using static Service.UserService;
 using Service;
 
 namespace testtest.Controllers
